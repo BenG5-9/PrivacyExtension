87adc015ce98dbed18b0dff97f0e19e160359dc0
Landon <l89@umbc.edu>
2024-04-24 15:23:30 -0400

    add comments in app.py

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,15 @@ CORS(app)
 @app.route('/home', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # get url from request
         url = request.get_json()['url']
-        print(url)
+
+        # find the policy
+        policy = extract_policy(url)
+
+        # Alan processes policy using the power of AI
+
+        # return modified data to be posted to the extension
         return extract_policy(url)
     
     else:
